b062a162a91875abef21fcac01e4c5258ca93f0c
pooja <p.mehra86@gmail.com>
2024-01-18 13:41:50 -0800

    path changes

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 });
 app.use('/api', routes);
 app.use(express.static(path.join(__dirname, '../build')));
-  app.get('/*', (req, res) => {
+  app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../build', 'index.html'));
   })
 const port = 8080;
