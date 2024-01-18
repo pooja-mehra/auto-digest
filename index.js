37951edb683e392d22312964aab19b561c63ed17
pooja <p.mehra86@gmail.com>
2024-01-18 13:29:19 -0800

    frontend build

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ app.use((req, res, next) => {
   next();
 });
 app.use('/api', routes);
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
   app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
   })
 const port = 8080;
 app.listen(port, (req,res) => {
