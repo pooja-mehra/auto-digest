9b3fa7f9df551d0f9d832847ba33f79709962abb
pooja <p.mehra86@gmail.com>
2024-01-18 21:39:34 -0800

    project name change

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 //app.use('/api', routes);
 //app.use(express.static(path.join(__dirname, '../build')));
   app.get('/', (req, res) => {
-    const msg = path.join(__dirname, '../build', 'index.html')
+    const msg = path.join(__dirname, '/build', 'index.html')
     //res.sendFile(path.join(__dirname, '../build', 'index.html'));
     res.json({success:msg})
   })
