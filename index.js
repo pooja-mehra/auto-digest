77bcd9d70f6d9eb31d4c5b817c66155201ec7fde
pooja <p.mehra86@gmail.com>
2024-01-18 22:28:02 -0800

    project name change

--- index.js
+++ index.js
@@ -15,15 +15,18 @@ mongoose
 mongoose.Promise = global.Promise;
 app.use(bodyParser.json());
 app.use(cors())
+app.use(cors({
+  origin: 'https://auto-digest.vercel.app'
+}));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
 //app.use('/api', routes);
-app.use(express.static(path.join(__dirname, '../../build')));
+app.use(express.static(path.join(__dirname, 'build')));
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   })
 const port = 8080;
 app.listen(port, (req,res) => {
