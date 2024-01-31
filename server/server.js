30123da9963e92c989c2a720260d4e255c46d921
pooja <p.mehra86@gmail.com>
2024-01-31 12:49:15 -0800

    seperate server

--- server/server.js
+++ server/server.js
@@ -16,23 +16,12 @@ mongoose
 mongoose.Promise = global.Promise;
 
 app.use(bodyParser.json());
+app.use(cors())
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
-app.use(cors())
 
 app.use('/api', routes);
 
